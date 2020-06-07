b983cef130c02bf36a4a3e0a5c1ece20e917890f
Glenn Cole <gcole@mac.com>
2020-06-06 23:48:39 -0700

    Initial Commit

---
+++ iOS-Triangles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-Triangles
+//
+//  Created by Glenn Cole on 6/6/20.
+//  Copyright © 2020 Glenn Cole. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
